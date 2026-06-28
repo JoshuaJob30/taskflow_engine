d4e42cc3f755d0abde0a625fc2e74ec5ddf237fa
Sampath Joshua Jeduthun Job <sa323311@wipro.com>
2026-06-28 20:30:13 +0530

    Add minikube setup

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  baseURL: "http://localhost:8080",
+  //baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
   headers: {
     "Content-Type": "application/json",
   },
